7685391f50cae839ccd92ec65751d02ad7206060
anunyaOrange <anunyaOrange@proton.me>
2025-06-19 18:31:36 +0700

    draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex flex-col items-center justify-center space-y-4 max-w-[300px] mx-auto">
+        <div className="flex flex-row w-[300px] h-[36px] justify-between items-center">
+          <div>Todo</div>
+          <div className="z-50">
+            <ModeToggle />
+          </div>
+        </div>
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border shadow-sm"
+          captionLayout="dropdown"
+        />
+        <Button>Click me</Button>
+        <Button>Click me</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
